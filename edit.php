0ee5f88dbfc4f22e817c5a532a264e939c847a14
migsilva89 <migdrum@gmail.com>
2021-12-20 03:47:33 +0100

    save edit 1st try!

--- edit.php
+++ edit.php
@@ -1,6 +1,5 @@
-<!-- AQUI VAMOS ADICIONAR CONEXAO DB PARA A TODO TABLE:: -->
+<!-- AQUI VAMOS ADICIONAR CONEXAO DB PARA A TODO_TABLE:: -->
 <?php 
-
     if(!empty($_GET['id']))
     {
         include_once('config.php');
@@ -31,20 +30,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>APP</title>
+    <title>EDIT</title>
 </head>
 <body>
 
-    <form action="system.php" method="POST">
+    <form action="save_edit.php" method="POST">
     <!-- ADD TODO FORM:: -->
         <br></br>
         <div class="inputBox">      
             <input type="text" name="addTodo" id="addTodo" class="inputTodo" value="<?php echo $task_desc?>" required>
-            <label for="addTodo" class="addTodo">Type your todo and submit</label>
+            <label for="addTodo" class="addTodo">Edit your todo and submit</label>
         </div>
-
         <br></br>
-        <input type="submit" name="submit" id="submit" placeholder="Submit">
+        <input type="submit" name="update" id="update">
     </form>
 
     <br></br>
---
+++ save_edit.php
@@ -0,0 +1,15 @@
+<?php
+
+    // if(isset($_POST['update']))
+
+    // include_once('config.php');
+    // {
+    //     $task_desc = $user_data['task_desc'];
+        
+    //     $sqlUpdate = "UPDATE todos_table SET task_desc='$task_desc' WHERE id='$id'";
+
+    //     $result = $conexao->query($sqlUpdate);
+    // }
+    // header('Location: system.php');
+
+?>
