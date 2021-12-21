53741de70a26f15fe741bb0a0d20ab73f2c9b293
migsilva89 <migdrum@gmail.com>
2021-12-21 02:26:20 +0100

    cli tail

--- partials/template-head.php
+++ partials/template-head.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TODOS APP</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <!-- <link href="/dist/styles.css?v=<?php //echo time()?>" rel="stylesheet"> -->
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+    <link href="/dist/styles.css" rel="stylesheet">
 </head>
 <body class="w-auto h-screen bg-cover" style="background-image: url('images/background_home.svg')">
   
