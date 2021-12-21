d9f076a1cf33b5e8ce7c3653cfb135052c942963
migsilva89 <migdrum@gmail.com>
2021-12-21 01:16:25 +0100

    styles to HOME VIEW!

--- partials/nav-guest.php
+++ partials/nav-guest.php
@@ -1,8 +1,26 @@
+<!-- NAVGUES == HOME.VIEW -->
 
 
-<h1 class="">Register at the APP</h1>
-<h2>JM- TODO LIST</h2>
-<div class="box">
-    <a href="login.php">Login</a>
-    <a href="register.php">Cadastre-se</a>
+<div>
+    <div class="md:p-24 pt-32 grid md:grid-cols-2 grid-cols-1 grid-rows-4">
+
+        <div class="text-white text-center md:text-left font-bold text-4xl ">
+            Controla as tuas tarefas!
+            <p>Gera a tua vida!</p>
+            Faz "tudo"
+            <br><br>
+            <hr class="hidden md:block">
+        </div>
+
+        <div class="md:text-right text-center md:absolute md:bottom-32 md:right-20 "> 
+            <div class="pb-16 text-8xl font-extrabold justify-self-start ">
+                FAZ TO-DO
+            </div>
+            <div class="text-xl drop-shadow">
+                <a class="order-last border-2 hover:bg-pink-400 hover:text-2xl hover:shadow-xl p-4 rounded-2xl text-white" href="login.php">Login</a>
+                <a class="border-2 p-4 hover:text-2xl hover:bg-pink-400 hover:shadow-xl rounded-2xl text-white" href="register.php">Cadastre-se</a>
+            </div>
+        </div>
+
+    </div>
 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   content: [
     "./**/*.php",
-    "./pages/**/*.{php,html}",
-    "./partials/**/*.{php,html}",
-    // more files here
   ],
   theme: {
     extend: {},
