55e91dad065a44a08c05326094d96c6074c0dd50
migsilva89 <migdrum@gmail.com>
2021-12-21 13:50:14 +0100

    login page view style done!

--- pages/login.php
+++ pages/login.php
@@ -2,18 +2,62 @@
 include (__DIR__.'/../partials/template-head.php');
 ?>
 
-<a href="home.php">Back</a>
-    <div>
-        <h1>Login</h1>
-        <!-- ADICIONAR METHOD POST PARA GUARDAR INFO  -->
-        <form method="POST" action="login.php">
-            <input type="text" name="email" placeholder="Email">
-            <br><br>
-            <input type="password" name="senha" placeholder="Senha">
-            <br><br>
-            <input class="inputSubmit" type="submit" value="Enviar" name="submit">
-        </form>
-    </div>
+
+
+
+        <div class="p-16 md:text-left text-center">
+            <a class="border-2 p-2 rounded-xl text-white hover:text-xl hover:bg-shadow-2xl bg-red-400 px-8" href="home.php">Back</a>
+        </div>
+
+        <div class="md:pt-64 drop-shadow-2xl flex justify-center items-center " >
+            <div class="grid-rows-2">
+
+                <!-- ROW 1 == IMAGEM -->
+                <div    
+                    class="rounded-t-xl bg-cover h-64 md:w-96 w-72 bg-no-repeat" 
+                    style="background-image: url('https://images.unsplash.com/photo-1501504905252-473c47e087f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8dGFzayx3b3JrLGxpc3R8fHx8fHwxNjQwMDg1MjI3&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080')"
+                >
+                </div>
+                
+                <!-- ROW 2 == CONTENT -->
+                <div class="bg-white md:text-left text-center rounded-b-xl">
+                    
+                    <form method="POST" action="login.php">
+                        <div class="pb-6 drop-shadow-2xl">
+                            <h1 class="text-4xl font-bold md:pl-4 pt-4"
+                            >Welcome
+                            </h1>
+                            <h2 class="md:pl-4"
+                            >Let's get started!
+                            </h2>
+                        </div>
+
+                        <div class="pl-4 pr-4 pb-8 drop-shadow-xl">
+                            <div class="pb-4">
+                                <input class="border-solid border-2 rounded-lg w-full  border-red-300 p-2" type="email" name="email" placeholder="Email">
+                            </div>
+                            <div>
+                                <input class="cursor-pointer border-solid border-2 rounded-lg w-full border-red-300 p-2" type="password" name="senha" placeholder="Senha">
+                            </div>
+                        </div>
+
+                        <div class="md:pl-4 pb-8  ">
+                            <input class="inputSubmit cursor-pointer  hover:bg-red-300 hover:shadow-xl border-2 p-2 rounded-xl text-white bg-red-400 px-8" type="submit" value="Login" name="submit">
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+        
+
+
+
+
+
+
+
+
 
 <?php
  include (__DIR__.'/../partials/template-head-close.php'); 
--- partials/nav-guest.php
+++ partials/nav-guest.php
@@ -1,9 +1,8 @@
-<!-- NAVGUES == HOME.VIEW -->
-
+<!-- HOME VIEW GUESTS-->
 
 <div class="w-auto h-screen bg-cover" style="background-image: url('images/background_home.svg')">
     <div class="md:p-24 pt-24 grid md:grid-cols-2 grid-cols-1 grid-rows-4">
-
+        <!-- COLUNA ESQUERDA  -->
         <div class="text-white text-center md:text-left font-bold md:text-4xl text-3xl">
             Controla as tuas tarefas!
             <p>Gere a tua vida!</p>
@@ -11,15 +10,17 @@
             <br><br>
             <hr class="hidden md:block">
         </div>
-
+        <!-- COLUNA DIREIRA -->
         <div class="md:text-right text-center md:absolute md:bottom-32 md:right-20 "> 
             <div class="pb-16 md:text-8xl text-6xl font-extrabold justify-self-start ">
                 FAZ TO-DO
             </div>
+            <!-- BUTTOES -->
             <div class="md:text-xl text-xm drop-shadow">
-                <a class="order-last border-2 hover:bg-pink-400 hover:text-2xl hover:shadow-xl p-4 rounded-2xl text-white" href="login.php">Login</a>
-                <a class="border-2 p-4 hover:text-2xl hover:bg-pink-400 hover:shadow-xl rounded-2xl text-white" href="register.php">Cadastre-se</a>
+                <a class="order-last border-2 hover:bg-red-400 hover:text-2xl hover:shadow-xl p-4 rounded-2xl text-white" href="login.php">Login</a>
+                <a class="border-2 p-4 hover:text-2xl hover:bg-red-400 hover:shadow-xl rounded-2xl text-white" href="register.php">Cadastre-se</a>
             </div>
         </div>
 
     </div>
+</div>
